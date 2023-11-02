996e4dcff0d2d58fc946889ba2c7813138a0502f
ggoyon <guillaume.goyon@capgemini.com>
2023-11-02 17:32:50 +0100

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,6 @@
 
 from setuptools import setup, find_packages
 from datetime import date
-import os
-
-
-def __path(filename):
-    ''''Build a full absolute path using the given filename
-
-        :params filename : filename to ass to the path of this module
-        :returns: full builded path
-    '''
-    return os.path.join(os.path.dirname(__file__), filename)
 
 
 with open('README.md') as f:
@@ -20,6 +10,9 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+# Read the requirements.in file and extract the required dependencies
+with open('requirements.in', 'r') as requirements_file:
+    reqs_list = [line.strip() for line in requirements_file if line.strip()]
 
 # Manage module version using date
 today = date.today()
@@ -27,37 +20,16 @@ today = date.today()
 # formating the date as yy.mm.dd
 version = today.strftime('%y.%m.%d')
 
-reqs_list = [
-    'nose2==0.9.1',
-    'rdflib==5.0.0',
-    'openpyxl==3.0.9',
-    'textdistance==4.2.0',
-    'python-gitlab==2.5.0',
-    'Flask==1.1.1',
-    'jsonpickle==1.4.1',
-    'requests==2.25.1',
-    'PyYAML==5.1.2',
-    'pandas==1.3.0',
-    'numpy==1.20.3',
-    'datetime==4.3',
-    'werkzeug==2.0.1',
-    'itsdangerous==2.0.1',
-    'table-logger==0.3.6',
-    'uvicorn==0.15.0',
-    'fastapi==0.70.0',
-             ]
-
 setup(
     name='ontology',
     version=version,
     description='Package containing the Ontology data and functions used in SoSTrades project',
     long_description=readme,
-    author='Airbus SAS',
-    url='https://idlvsrv284.eu.airbus.corp/sostrade/ontology.git',
+    author='Airbus SAS, Capgemini',
+    url='https://github.com/os-climate/sostrades-ontology',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     include_package_data=True,
-    python_requires='>=3.7',
-    install_requires=reqs_list
+    python_requires='>=3.9',
+    install_requires=reqs_list,
 )
-
