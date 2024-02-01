4a0b9a59e643d2fa50d24e224a80bb112184fa2b
Eric LOQUET <eric.loquet@capgemini.com>
2024-02-01 20:27:23 +0100

    Improve header check move l1s headers to l0 add a script to auto complete headers
    Signed-off-by: Eric LOQUET <eric.loquet@capgemini.com>

--- sos_ontology/tests/l0_test_header.py
+++ sos_ontology/tests/l0_test_header.py
@@ -1,5 +1,5 @@
 '''
-Copyright 2023 Capgemini
+Copyright 2024 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -16,10 +16,11 @@ limitations under the License.
 '''
 mode: python; py-indent-offset: 4; tab-width: 8; coding:utf-8
 '''
+
+import HeadersIgnoreConfig
+from sostrades_core.tools.check_headers import HeaderTools
 import unittest
 import pprint
-from sostrades_core.tools.check_headers import check_headers
-
 
 class Testheader(unittest.TestCase):
     """
@@ -31,14 +32,12 @@ class Testheader(unittest.TestCase):
         Initialize third data needed for testing
         '''
         self.pp = pprint.PrettyPrinter(indent=4, compact=True)
-        self.ExtensionToIgnore = ["xlsx","owl","pkl", "png", "jpg", "csv", "md", "markdown", "avif", "json", "in", "gitignore", "cfg", "puml", "pdf", "txt", "ipynb", "zip", "rst"]
-        #Add here the files to ignore    
-        self.FilesToIgnore = []
+        self.extension_to_ignore = HeadersIgnoreConfig.extension_to_ignore
+        #Add here the files to ignore  
+        self.files_to_ignore = HeadersIgnoreConfig.files_to_ignore
         #commit from where to compare added, modeified deleted ...
-        self.airbus_rev_commit = "9ce0e11f2dfcef0a8b6b5079932249b363ea3d5b"
+        self.airbus_rev_commit = HeadersIgnoreConfig.airbus_rev_commit
 
     def test_Headers(self):
-        check_headers(self.ExtensionToIgnore,self.FilesToIgnore,self.airbus_rev_commit)
-
-        
-
+        ht = HeaderTools()
+        ht.check_headers(self.extension_to_ignore, self.files_to_ignore, self.airbus_rev_commit)
