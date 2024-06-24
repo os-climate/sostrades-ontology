0119b6970fc4ef81d99cb87b9f32f116b6d04b4e
GOYON Guillaume <guillaume.goyon@capgemini.com>
2024-06-24 14:42:06 +0200

    Ruff fixes

--- sos_ontology/core/sos_terminology.py
+++ sos_ontology/core/sos_terminology.py
@@ -1,6 +1,6 @@
 '''
 Copyright 2022 Airbus SAS
-Modifications on 2024/05/16 Copyright 2024 Capgemini
+Modifications on 2024/05/16-2024/06/24 Copyright 2024 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -85,7 +85,7 @@ class SoSTerminology:
                     row_dict[headers[j]] = row[j]
                 if row[j] is not None and row[j] != '' and str(row[j])[0:1] != '=':
                     isNone = False
-            if isNone == False:
+            if not isNone:
                 if nameDictKey != '':
                     sheetDict[row[indexDictKey]] = row_dict
                 else:
